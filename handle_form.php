ad726e652f3a6d32b3fa74b8ce2013d2313a485d
ReginaldSwanJr <swan2011@gmail.com>
2018-08-30 23:05:22 +0000

    lab1

---
+++ handle_form.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Form Feedback</title>
+</head>
+<body>
+<?php # Script 2.5 - handle_form.php #4
+
+// Create one array:
+$states = [
+	'1' => 'Alabama',
+	'2' => 'Florida',
+	'3' => 'Georgia',
+	'4' => 'Mississippi',
+	'5'	=> 'South Carolina'
+];
+
+// Print the submitted information:
+if ( !empty($_POST['lName']) && !empty($_POST['fName']) && !empty($_POST['comments']) && !empty($_POST['email']) ) {
+	echo "<p>Thank you, <strong>{$_POST['fName']}</strong>, for the following comments:</p>
+<pre>{$_POST['comments']}</pre>
+<p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
+} else { // Missing form value.
+	echo '<p>Please go back and fill out the form again.</p>';
+	#echo '<p>How is the weather in </p>'. $states[3] . '<p>?</p>'
+}
+?>
+</body>
+</html>
