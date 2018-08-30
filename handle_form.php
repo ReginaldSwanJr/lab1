f07216a3fd843331c24ccbc58142da144807b357
ReginaldSwanJr <swan2011@gmail.com>
2018-08-30 23:18:13 +0000

    Changed the php for the array

--- handle_form.php
+++ handle_form.php
@@ -17,10 +17,11 @@ $states = [
 ];
 
 // Print the submitted information:
-if ( !empty($_POST['lName']) && !empty($_POST['fName']) && !empty($_POST['comments']) && !empty($_POST['email']) ) {
+  if ( !empty($_POST['lName']) && !empty($_POST['fName']) && !empty($_POST['comments']) && !empty($_POST['email']) ) {
 	echo "<p>Thank you, <strong>{$_POST['fName']}</strong>, for the following comments:</p>
-<pre>{$_POST['comments']}</pre>
-<p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
+	<pre>{$_POST['comments']}</pre>
+	<p>How is the weather in <em>$states[3]</em>?</p>
+	<p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
 } else { // Missing form value.
 	echo '<p>Please go back and fill out the form again.</p>';
 	#echo '<p>How is the weather in </p>'. $states[3] . '<p>?</p>'
