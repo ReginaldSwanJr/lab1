e6241e493566d7ff054b002b44fd5745bc74c206
ReginaldSwanJr <swan2011@gmail.com>
2018-09-04 22:15:30 +0000

    fixed the drop down menu

--- handle_form.php
+++ handle_form.php
@@ -13,18 +13,22 @@ $states = [
 	'2' => 'Florida',
 	'3' => 'Georgia',
 	'4' => 'Mississippi',
-	'5'	=> 'South Carolina'
-];
+	'5'	=> 'South Carolina'];
+
+$sstate = $_POST['state'];
 
 // Print the submitted information:
-  if ( !empty($_POST['lName']) && !empty($_POST['fName']) && !empty($_POST['comments']) && !empty($_POST['email']) ) {
-	echo "<p>Thank you, <strong>{$_POST['fName']}</strong>, for the following comments:</p>
-	<pre>{$_POST['comments']}</pre>
-	<p>How is the weather in <em>$states[3]</em>?</p>
-	<p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
-} else { // Missing form value.
-	echo '<p>Please go back and fill out the form again.</p>';
-	#echo '<p>How is the weather in </p>'. $states[3] . '<p>?</p>'
+  if ( !empty($_POST['lName']) && !empty($_POST['fName']) && !empty($_POST['comments']) && !empty($_POST['email']) ) 
+	{
+		echo "<p>Thank you, <strong>{$_POST['fName']}</strong>, for the following comments:</p>
+		<pre>{$_POST['comments']}</pre>
+		<p>How is the weather in $states[$sstate] ?</p>
+		<p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
+	}
+	else 
+	{ // Missing form value.
+		echo '<p>Please go back and fill out the form again.</p>';
+		#echo '<p>How is the weather in </p>'. $states[3] . '<p>?</p>'
 }
 ?>
 </body>
